edfd7fba6666060d6a70b3e9f28aacb57e4dad6f
dattatreya303 <chikumhptr@gmail.com>
2018-03-01 01:46:48 +0530

    Remove executable from git

--- test_matrix.cpp
+++ test_matrix.cpp
@@ -21,6 +21,14 @@ int main(int argc, char const *argv[]) {
     // test::TestComplexMultiplyDifferent();
     // test::TestComplexMultiplyAssign();
 
+    smps::Matrix< std::complex<int> > C(1000, 1000);
+    for( int i = 0; i < 1000; i++ ) {
+        for( int j = 0; j < 1000; j++ ) {
+            C.insert_element( i, j, std::complex<int>( rand()%10, rand()%10 ) );
+        }
+    }
+    smps::Matrix< std::complex<int> > D(C);
+
     smps::Matrix<int> A(1000, 1000);
     for( int i = 0; i < 1000; i++ ) {
         for( int j = 0; j < 1000; j++ ) {
@@ -33,23 +41,8 @@ int main(int argc, char const *argv[]) {
     A *= B;
     const auto end_time = std::chrono::steady_clock::now();
     
-    std::cout << "Matrix<int>(1000,1000): Multiplication: Execution time = " <<
+    std::cout << "Matrix(1000,1000): Multiplication: Execution time = " <<
     std::chrono::duration<double, std::milli>(end_time - start_time).count() / 1000 << " seconds" << std::endl;
-
-    smps::Matrix< std::complex<int> > C(1000, 1000);
-    for( int i = 0; i < 1000; i++ ) {
-        for( int j = 0; j < 1000; j++ ) {
-            C.insert_element( i, j, std::complex<int>( rand()%10, rand()%10 ) );
-        }
-    }
-    smps::Matrix< std::complex<int> > D(C);
-
-    // start_time = std::chrono::steady_clock::now();
-    C *= D;
-    // end_time = std::chrono::steady_clock::now();
-    
-    // std::cout << "Matrix< complex<int> >(1000,1000): Multiplication: Execution time = " <<
-    // std::chrono::duration<double, std::milli>(end_time - start_time).count() / 1000 << " seconds" << std::endl;
 
     return 0;
 }
